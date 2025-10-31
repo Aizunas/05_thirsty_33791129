a253f6ee2dfbdcadc29eff7c99943a7a45c44120
“Sanuzia <sanuziajorge@gmail.com>
2025-10-31 00:53:49 +0000

    Changes to index.js file

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+// Setup express and ejs
 const express = require('express');
 const ejs = require('ejs');
 
+// Create the express application object
 const app = express();
-const port = 8000;
 
-// Tell Express to use EJS
-app.set('view engine', 'ejs');
-
-// Parse POST request body
+// Set up the body parser for POST requests
 app.use(express.urlencoded({ extended: true }));
 
+// Tell Express that we want to use EJS as the templating engine
+app.set('view engine', 'ejs');
+
 // Load the route handlers
-const mainRoutes = require("./routes/main");  
+const mainRoutes = require('./routes/main');
 app.use('/', mainRoutes);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Use the port provided by the VM or default to 8000
+const port = process.env.PORT || 8000;
 
-app.use(express.static('public'));
-
+// Start the web app listening
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+});
